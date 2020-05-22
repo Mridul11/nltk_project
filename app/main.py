b118275ed98ce422f930fbc96e84294ff73743da
= <mridulmishra2991@gmail.com>
2020-05-22 19:44:16 +0530

    added stem processing

---
+++ app/main.py
@@ -0,0 +1,13 @@
+import nltk 
+from nltk.stem import PorterStemmer 
+from nltk.tokenize import word_tokenize 
+
+ps = PorterStemmer()
+
+example_words = ["python", "pythoner", "pythoning", "pythonicness"]
+new_text = "It is very important to be pythonly while you are pythoning with python and pythonicness is amazing!"
+words = word_tokenize(new_text)
+
+def running():
+    for w in words:
+        print(ps.stem(w))
