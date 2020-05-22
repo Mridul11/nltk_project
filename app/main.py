616cb6036c769a7d0aad1972e69d8bf9abb55e39
Mridul11 <mridulmishra2991@gmail.com>
2020-05-22 20:37:45 +0530

    added new word

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from nltk.tokenize import word_tokenize
 ps = PorterStemmer()
 
 example_words = ["python", "pythoner", "pythoning", "pythonicness"]
-new_text = "It is very important to be pythonly while you are pythoning with python and pythonicness is amazingly awesome in a python way!"
+new_text = "It is so important to be pythonly while you are pythoning with python and pythonicness is amazingly awesome in a python way!"
 words = word_tokenize(new_text)
 
 def running():
